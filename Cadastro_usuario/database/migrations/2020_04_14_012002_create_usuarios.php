31c48a691b4fdf4d283b168dfc10cd7a2eb74aae
david1267 <62625257+david1267@users.noreply.github.com>
2020-04-13 22:42:27 -0300

    Add files via upload

---
+++ Cadastro_usuario/database/migrations/2020_04_14_012002_create_usuarios.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsuarios extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->id();
+            $table->string("Nome", 150);
+            $table->string("Login", 20);
+            $table->string("Senha", 20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuarios');
+    }
+}
